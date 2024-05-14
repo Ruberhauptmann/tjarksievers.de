e1f25d786b6b648c5d82cf9d949fed0a7d395af6
Tjark Sievers <tjarksievers@icloud.com>
2024-05-14 22:34:38 +0200

    Fix submodule paths

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 files=(
   docker-compose.yml
   pages/tjarksievers.de/docker-compose.yml
+  pages/budget.tjarksievers.de/docker-compose.yml
+  #pages/api.saboga.tjarksievers.de/docker-compose.yml
+  #pages/saboga.tjarksievers.de/docker-compose.yml
 )
 
 COMPOSE_FILE=$( IFS=:; printf '%s' "${files[*]}" )
