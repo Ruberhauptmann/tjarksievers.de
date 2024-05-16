94cbaa3b7cf5e09807ee2b78121e183fe49e7213
Tjark Sievers <tjarksievers@icloud.com>
2024-05-16 18:25:23 +0200

    Deploy saboga API

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ files=(
   docker-compose.yml
   pages/tjarksievers.de/docker-compose.yml
   pages/budget.tjarksievers.de/docker-compose.yml
-  #pages/api.saboga.tjarksievers.de/docker-compose.yml
+  pages/api.saboga.tjarksievers.de/docker-compose.yml
   #pages/saboga.tjarksievers.de/docker-compose.yml
 )
 
