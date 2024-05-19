ff68c7eaf3087a2ccbf70b05c509e0a9952c7d73
Tjark Sievers <tjarksievers@icloud.com>
2024-05-19 12:21:59 +0200

    Add backup service for mongodb

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,6 @@ files=(
 COMPOSE_FILE=$( IFS=:; printf '%s' "${files[*]}" )
 export COMPOSE_FILE
 
-mkdir /mgob
-mkdir /mgob/config
-mkdir /mgob/storage
-mkdir /mgob/tmp
-mkdir /mgob/data
-cp pages/api.saboga.tjarksievers.de/backup.yaml /mgob/config/backup.yaml
-
 docker --context=production compose stop
 docker --context=production compose pull
 docker --context=production compose up -d --build
-
