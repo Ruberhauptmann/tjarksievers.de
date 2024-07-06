ac7300c893ab780b163f6ee1b26c40b33f44cb44
Tjark Sievers <tjarksievers@icloud.com>
2024-07-06 19:39:38 +0200

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ files=(
   pages/tjarksievers.de/docker-compose.yml
   pages/sunflower.tjarksievers.de/docker-compose.yml
   pages/budget.tjarksievers.de/docker-compose.yml
-  pages/api.saboga.tjarksievers.de/docker-compose.yml
   pages/saboga.tjarksievers.de/docker-compose.yml
 )
 
