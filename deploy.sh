2cd8e903a707d98764c8afddee8fe1be5ccf128f
Tjark Sievers <tjarksievers@icloud.com>
2025-03-22 20:28:04 +0100

    Add prometheus (for real)

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ files=(
   pages/tjarksievers.de/docker-compose.yml
   pages/sunflower.tjarksievers.de/docker-compose.yml
   pages/saboga.tjarksievers.de/docker-compose.yml
+  pages/monitoring.tjarksievers.de/docker-compose.yml
 )
 
 COMPOSE_FILE=$( IFS=:; printf '%s' "${files[*]}" )
