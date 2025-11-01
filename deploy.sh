c12b1622710605ae4a57f3afdb330918e31fb2c1
Tjark Sievers <tjarksievers@icloud.com>
2025-11-01 13:07:35 +0100

    Add MTG timeline game

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,3 @@ docker --context=production compose stop
 
 echo "Deploying..."
 docker --context=production compose up -d --build
-
-ssh deploy@tjarksievers.de "docker cp /srv/mtg-timeline-game/src/. mtg-tjarksievers-de:/usr/share/nginx/html/"
-
--- push_mtg_game.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-
-rsync -avz --progress pages/mtg.tjarksievers.de/src tjarksievers.de:/srv/mtg-timeline-game
